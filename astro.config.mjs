7bff760fbd834883d2202ab5162108c980141f2e
Edgar <jnzokwe@gmail.com>
2026-07-04 16:37:05 -0400

    use custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://dedgar1.github.io',
+	site: 'https://straypackets.com',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
