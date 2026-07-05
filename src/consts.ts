be367db4980aa75ab80bc9b22f3c76b4f947bae2
Edgar <jnzokwe@gmail.com>
2026-07-05 14:49:13 -0400

    update headers, footers, and add custom favicon

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Straypackert';
+export const SITE_DESCRIPTION = 'Stray thoughts, tech musings, and everything in between.';
