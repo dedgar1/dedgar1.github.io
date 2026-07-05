d3d022a08996d70a77ef6853326af9de0cb8e62d
Edgar <jnzokwe@gmail.com>
2026-07-05 16:22:43 -0400

    change site name to straypackets

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Straypackert';
+export const SITE_TITLE = 'Straypackets';
 export const SITE_DESCRIPTION = 'Stray thoughts, tech musings, and everything in between.';
